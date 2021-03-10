c9e556582ee3d755b2a43b842a94e402dd6879ec
John McDonald <jmcdonald1991@gmail.com>
2021-03-10 01:15:47 -0600

    latest stuff

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,3 +1,12 @@
 import { User } from "./user";
+import { Company } from "./company";
 
 const user = new User();
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
