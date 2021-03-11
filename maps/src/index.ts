eba273e2367dab8b8f9b1eb7d3f6abdd37dd0e1a
John McDonald <jmcdonald@iabbb.org>
2021-03-10 19:23:02 -0600

    markers

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -3,5 +3,8 @@ import { Company } from './company';
 import { CustomMap } from './CustomMap';
 
 const customMap = new CustomMap('map');
-
 const user = new User();
+const company = new Company();
+
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
