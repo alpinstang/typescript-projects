68e8e949bca1a3e4dc2c46515ebdf2eb499dc57b
John McDonald <jmcdonald1991@gmail.com>
2021-03-11 00:16:38 -0600

    addMarker interface

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,6 +1,13 @@
 import { User } from './user';
 import { Company } from './company';
 
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -14,22 +21,12 @@ export class CustomMap {
     });
   }
 
-  addUserMarker(user: User): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
-      },
-    });
-  }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
